81b22d3a5aa72e9f0090a2858868f9f942432edf
Иван Бакаидов <aacidov@Air-Ivan.Dlink>
2017-03-23 13:03:49 +0300

    0.1.0 add callback

--- README.md
+++ README.md
@@ -4,6 +4,6 @@ TTS module for windows
 ## Usage
 ``js
     const wintts = require('wintts')
-    wintts('Hello, world!')
+    wintts('Hello, world!', (err)=>{})
 
 ``
--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const execFile = require('child_process').execFile;
 const sep = require('path').sep;
 const bin = __dirname+sep+'tts.exe';
 
-function say(text) {
+function say(text, cb) {
     
-    execFile(bin, [text])
+    execFile(bin, [text], cb)
 
 }
 
